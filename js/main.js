917e09f13a1c04fe5b12897308a9ee44d1770673
swoerkom <swoerkom@spartaglobal.com>
2019-04-19 13:31:53 +0100

    Spiders added, CSS changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function () {
-
   var score = 0;
   var gameEnd = false;
   var walls = $(".wall");
@@ -7,11 +6,11 @@ $(function () {
   var item = $(".item");
 
   //Hide elements on launch
-  $("#maze_container").hide();
   $("#level-menu").hide();
   $("#finish_screen").hide();
   $("#game_buttons").hide();
   $("#how-to-play-screen").hide();
+  $("#game_buttons").show();
 
   //Place character on maze board
   function placeCharacter() {
@@ -27,12 +26,14 @@ $(function () {
       var randomnumber=Math.floor(Math.random()*divCount);
       $(floor[randomnumber]).append('<img class="item coin" src="images/coin.png">');
     }
-
-    for (var i = 0; i < 10; i++) {
-      var divCount = $(walls).length;
-      var randomnumber=Math.floor(Math.random()*divCount);
-      $(floor[randomnumber]).append('<img class="item diamond" src="images/diamond.png">');
-    }
+      for (var i = 0; i < 10; i++) {
+        var randomnumber=Math.floor(Math.random()*divCount);
+        $(floor[randomnumber]).append('<img class="item diamond" src="images/diamond.png">');
+      }
+      for (var i = 0; i < 7; i++) {
+        var randomnumber=Math.floor(Math.random()*divCount);
+        $(floor[randomnumber]).append('<img class="spider" src="images/spider.png">');
+      }
   }
 
   //End game when player reaches exit div
@@ -43,7 +44,7 @@ $(function () {
       }
     } else {
       gameEnd = false;
-    }
+      }
   }
 
   //Check player position using jQuery Collison plugin
@@ -66,43 +67,52 @@ $(function () {
       if (coinHit[0] != undefined) {
         $(coinHit[0]).remove();
           score = score - 3;
+          $('<p class="game_message">Coin Hit -3 points!</p>').appendTo("#maze_container").hide().fadeToggle(500);
       } if (diamondHit[0] != undefined) {
         $(diamondHit[0]).remove();
+        $('<p class="game_message">Diamond Hit -6 points!</p>').appendTo("#maze_container").hide().fadeToggle(500);
+
         score = score - 6;
       }
-  }
+    //If they have hit a spider
+    var spiderHit = $("#character").collision(".spider");
+    if (spiderHit[0] != undefined) {
+      $(spiderHit[0]).remove();
+      $('<p class="game_message">Spider Hit +10 points!</p>').appendTo("#maze_container").hide().fadeToggle(500);
+
+      score = score + 10;
+      }
+
+      $( ".game_message" ).fadeOut( "100", function() {
+    });
+}
+
+
 
   //Displays player score on finish
   function playerScore() {
-    $( ".stars" ).html('Your final score is: ' +score);
+    $(".stars").append('Score: ' +score).css("display","inline-block");
     if (score <= 60) {
       $( ".stars" ).append('<h4>Well done, you won 3 stars!</h4>');
       $( ".stars" ).append('<img src="images/star.png"><img src="images/star.png"><img src="images/star.png">');
   } else if (score > 60 && score <= 100) {
-       $( ".stars" ).append('<h4>You won 2 stars, better luck next time!</h4>');
-       $( ".stars" ).append('<img src="images/star.png"><img src="images/star.png"><img src="images/star-grey.png">');
+      $( ".stars" ).append('<h4>You won 2 stars, better luck next time!</h4>');
+      $( ".stars" ).append('<img src="images/star.png"><img src="images/star.png"><img src="images/star-grey.png">');
   } else if (score > 100) {
       $( ".stars" ).append('<h4>You only won 1 star, try harder!</h4>');
       $( ".stars" ).append('<img src="images/star.png"><img src="images/star-grey.png"><img src="images/star-grey.png">');
     }
     $("#finish_screen").show();
-    $("#maze_container").hide();
     $("#game_buttons").hide();
-    $(".restart-game").on("click", function () {
-      $(character).remove();
-      placeCharacter();
-  }) }
-
-  placeItems();
-  $(".score").append('Score: ' +score).css("display","inline-block");
+    $("#maze_container").hide();
+  }
 
   //MOVE CHARACTER
   function moveCharacter() {
    $(document).keydown(function(e) {
       var position = $("#character").position();
       var value = true;
-        switch(e.keyCode)
-        {
+        switch(e.keyCode) {
           case 40: //DOWN
           $("#character").css('top', position.top + 20 + 'px');
             break;
@@ -115,83 +125,56 @@ $(function () {
           case 39: //RIGHT
             $("#character").css('left', position.left + 20 + 'px');
             break;
+        }
+        checkItem();
+        if (checkPosition()) {
+          $("#character").css('top', position.top + 'px');
+          $("#character").css('left', position.left + 'px');
+        } else {
+          score++
           }
-          checkItem();
-          if (checkPosition()) {
-            $("#character").css('top', position.top + 'px');
-            $("#character").css('left', position.left + 'px');
-          } else {
-            score++
-          }
-      $(".score").html('Score: ' +score);
-      endGame();
-      if (gameEnd == true) {
-                playerScore();
-                score =+ 0;
-      }
-        })
+        $(".score").html('Score: ' +score);
+
+        endGame();
+        if (gameEnd === true) {
+          $(character).remove();
+          playerScore();
+        }
+   })
   }
 
   //Game buttons
-    $(".start-button").on("click", function () {
-      $("#level-menu").show();
-      $("#start-menu").hide();
-      $("#game_buttons").hide();
-      $( ".stars" ).html("");
-      $(character).remove();
-      placeCharacter();
-    })
+  $(".start-button").on("click", function () {
+    $("#level-menu").show();
+    $("#start-menu").hide();
+    $("#game_buttons").hide();
+  })
 
+  $(".back-to-menu-button").on("click", function () {
+    $("#start-menu").show();
+    $("#finish_screen").hide();
+    $("#how-to-play-screen").hide();
+    $("#level-menu").hide();
+    $("#game_buttons").hide();
+  })
 
-    $(".level2-button").on("click", function () {
-      $("#maze_container").show();
-      $("#level-menu").hide();
-      $("#start-menu").hide();
-      $("#game_buttons").show();
-      $( ".stars" ).html("");
-      $(character).remove();
-      placeCharacter();
-    })
+  $(".restart-game").on("click", function () {
+    $(character).remove();
+    $(".item").remove();
+    placeCharacter();
+    placeItems();
+    score =+ 0;
+    $(".score").html('Score: ' +score);
+  })
 
+  $(".how-to-play-button").on("click", function() {
+    $("#start-menu").hide();
+    $("#how-to-play-screen").show();
+  })
 
-    $(".play-again").on("click", function () {
-      $( ".stars" ).html("");
-      $(character).remove();
-      placeCharacter();
-      $("#maze_container").show();
-      $("#finish_screen").hide();
-      $("#game_buttons").show();
+  placeItems();
+  $(".score").append('Score: ' +score).css("display","inline-block");
+  placeCharacter();
+  moveCharacter();
 
-    })
-
-    $(".back-to-menu-button").on("click", function () {
-      $("#maze_container").hide();
-      $("#start-menu").show();
-      $("#finish_screen").hide();
-      $("#game_buttons").hide();
-      $("#how-to-play-screen").hide();
-      score =+ 0;
-      $(".score").html('Score: ' +score);
-      placeCharacter();
-      placeItems();
-
-    })
-
-    $(".restart-game").on("click", function () {
-      $(character).remove();
-      $(".item").remove();
-      placeCharacter();
-      placeItems();
-      score =+ 0;
-      $(".score").html('Score: ' +score);
-    })
-
-    $(".how-to-play-button").on("click", function() {
-      $("#maze_container").hide();
-      $("#start-menu").hide();
-      $("#how-to-play-screen").show();
-    })
-
-      placeCharacter();
-      moveCharacter();
 });
